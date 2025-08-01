a0cbbca0b7069af1813474b8b5b4232016da2ac1
kimdonghyeon <dhb6306@gmail.com>
2025-08-01 12:14:01 +0900

    user->member로 수정

--- everyday/src/main/java/com/nyam/everyday/module/user/repository/MembersRepository.java
+++ everyday/src/main/java/com/nyam/everyday/module/user/repository/MembersRepository.java
@@ -1,10 +1,10 @@
 package com.nyam.everyday.module.user.repository;
 
-import com.nyam.everyday.module.user.entity.Users;
+import com.nyam.everyday.module.user.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users,Long> {
+public interface MembersRepository extends JpaRepository<Members,Long> {
 
 }
