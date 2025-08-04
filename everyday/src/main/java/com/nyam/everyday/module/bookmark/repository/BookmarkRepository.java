9471c009598f83043d6aaf7445f48f26fc1416ac
kimdonghyeon <dhb6306@gmail.com>
2025-08-04 11:06:40 +0900

    repository-BookmarkRepository생성

---
+++ everyday/src/main/java/com/nyam/everyday/module/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,10 @@
+package com.nyam.everyday.module.bookmark.repository;
+
+import com.nyam.everyday.module.bookmark.entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
+
+}
