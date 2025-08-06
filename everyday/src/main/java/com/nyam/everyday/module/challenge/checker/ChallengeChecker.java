2f6cd086f050206c398e073b4b0f413caf90a8d3
hojong <33508056+LteFroggy@users.noreply.github.com>
2025-08-06 11:58:39 +0900

    cleanup : ChallengeChecker 주석 추가 (#35)

--- everyday/src/main/java/com/nyam/everyday/module/challenge/checker/ChallengeChecker.java
+++ everyday/src/main/java/com/nyam/everyday/module/challenge/checker/ChallengeChecker.java
@@ -7,8 +7,9 @@ import com.nyam.everyday.module.member.entity.Member;
 // 체크를 위해 필요한 기능들이 포함된 체커
 public interface ChallengeChecker {
 
-  // 타입 반환
+  // 타입 반환 함수 (어떤 태그의 챌린지인지 확인하기 위함)
   ChallengeType getSupportedType();
 
+  // 챌린디 잘성 여부를 체크하는 함수
   Boolean check(Member member);
 }
