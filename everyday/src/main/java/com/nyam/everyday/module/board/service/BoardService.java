01cfb2114f0604e26936741b8c148886894a7f70
kimdonghyeon <dhb6306@gmail.com>
2025-08-08 11:31:33 +0900

    Board 게시글 엔티티-> 댓글 기능 확장 추가 게시글 컨트롤러-게시글 생성 컨트롤러 추가 게시글 서비스->게시글 서비스 로직 추가 게시글 dto->게시글 생성 request,response추가

--- everyday/src/main/java/com/nyam/everyday/module/board/service/BoardService.java
+++ everyday/src/main/java/com/nyam/everyday/module/board/service/BoardService.java
@@ -1,11 +1,48 @@
 package com.nyam.everyday.module.board.service;
 
+import com.nyam.everyday.common.exception.BaseException;
+import com.nyam.everyday.common.exception.ErrorCode;
+import com.nyam.everyday.module.board.entity.Board;
+import com.nyam.everyday.module.board.repository.BoardRepository;
+import com.nyam.everyday.module.member.entity.Member;
+import com.nyam.everyday.module.member.entity.Status;
+import com.nyam.everyday.module.member.repository.MemberRepository;
+import com.nyam.everyday.web.board.dto.BoardResponseDto;
+import com.nyam.everyday.web.board.dto.CreateBoardRequestDto;
+import com.nyam.everyday.web.board.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
+
+  private final BoardRepository boardRepository;
+  private final MemberRepository memberRepository;
+  private final BoardMapper boardMapper;
+
+  //게시글 생성 메서드
+  @Transactional
+  public BoardResponseDto createBoard(CreateBoardRequestDto dto,Long memberId) {
+
+    //1. 작성자 조회
+    Member member = memberRepository.findById(memberId)
+        .orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND));
+
+    if (member.getMemberStatus() == Status.DEACTIVATED){
+      throw new BaseException(ErrorCode.MEMBER_DEACTIVATED);
+    }
+
+    Board board = boardMapper.toEntity(dto, member);
+    Board saved =  boardRepository.save(board);
+    return boardMapper.toDto(saved);
+
+
+
+
+
+  }
+  public void deleteBoard(Long boardId) {}
 
 }
--- everyday/src/main/java/com/nyam/everyday/web/board/controller/BoardController.java
+++ everyday/src/main/java/com/nyam/everyday/web/board/controller/BoardController.java
@@ -1,14 +1,42 @@
 package com.nyam.everyday.web.board.controller;
 
+import com.nyam.everyday.module.board.service.BoardService;
+import com.nyam.everyday.security.core.CustomUserDetails;
+import com.nyam.everyday.web.board.dto.BoardResponseDto;
+import com.nyam.everyday.web.board.dto.CreateBoardRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/boards")
 @Slf4j
 @RequiredArgsConstructor
 public class BoardController {
+
+  private final BoardService boardService;
+
+  @Operation(summary = "게시글 작성",description = "게시판에 게시글을 작성합니다")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "게시글 작성 성공"),
+      @ApiResponse(responseCode = "404",description = "사용자 정보 없음"),
+      @ApiResponse(responseCode = "400",description = "잘못된 요청 방식")
+  })
+  @PostMapping("/post")
+  public ResponseEntity<BoardResponseDto> createBoard(@Valid @RequestBody CreateBoardRequestDto dto
+      ,@AuthenticationPrincipal CustomUserDetails userDetails) {
+    BoardResponseDto response = boardService.createBoard(dto, userDetails.getId());
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
 
 }
