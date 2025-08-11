eabc1f6b3994aafd426c7ad1d6311dc6d812230c
LteFroggy <zac0328@naver.com>
2025-08-11 11:32:25 +0900

    feat : WebsocketConfig 파일 추가 (#41)

---
+++ everyday/src/main/java/com/nyam/everyday/config/websocket/WebSocketConfig.java
@@ -0,0 +1,43 @@
+package com.nyam.everyday.config.websocket;
+
+import com.nyam.everyday.config.websocket.interceptor.RoomSubscribeAuthInterceptor;
+import com.nyam.everyday.config.websocket.interceptor.StompAuthConnectInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final RoomSubscribeAuthInterceptor roomSubscribeAuthInterceptor;
+  private final StompAuthConnectInterceptor stompAuthConnectInterceptor;
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws") // 클라이언트가 연결할 엔드포인트
+        .setAllowedOrigins("http://localhost:5173") // CORS 허용
+        // .setHandshakeHandler(new CustomHandshakeHandler())  // 직접 생성한 Handler 추가
+        // .addInterceptors(new CustomHandshakeInterceptor())  // 직접 생성한 Interceptor 추가
+        .withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 메시지 보낼 때 prefix
+    registry.enableSimpleBroker("/topic", "/queue"); // 서버가 메시지 보낼 때 prefix
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(
+        stompAuthConnectInterceptor,
+        roomSubscribeAuthInterceptor
+    );
+  }
+}
