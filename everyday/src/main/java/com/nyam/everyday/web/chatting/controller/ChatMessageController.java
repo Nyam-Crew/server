1eb53e70d3361d576f05434975c80d60f53581fd
LteFroggy <zac0328@naver.com>
2025-08-12 16:22:06 +0900

    feat : 채팅 메세지 관련 기능 처리를 위한 컨트롤러 추가 (#41)

--- everyday/src/main/java/com/nyam/everyday/web/chatting/controller/ChatMessageController.java
+++ everyday/src/main/java/com/nyam/everyday/web/chatting/controller/ChatMessageController.java
@@ -1,20 +1,20 @@
 package com.nyam.everyday.web.chatting.controller;
 
-import com.nyam.everyday.module.chatting.mongo.entity.ChatMessage;
-import com.nyam.everyday.module.chatting.mongo.service.ChatMessageService;
+import com.nyam.everyday.module.chatting.chatmessage.mongo.entity.ChatMessage;
+import com.nyam.everyday.module.chatting.chatmessage.service.ChatMessageService;
+import com.nyam.everyday.security.core.CustomUserDetails;
+import com.nyam.everyday.web.chatting.dto.ChatMessageBroadcastDto;
 import com.nyam.everyday.web.chatting.dto.ChatMessageSaveRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,19 +27,27 @@ public class ChatMessageController {
 
   @PostMapping("/{send}")
   @Operation(summary = "채팅 저장하기")
-  public ChatMessage sendMessage(@RequestBody ChatMessageSaveRequest chatMessage) {
-    return chatMessageService.save(chatMessage);
+  public void sendMessage(@RequestBody ChatMessageSaveRequest request,
+      @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    Long memberId = customUserDetails.getId();
+
+    chatMessageService.handleMessage(request, memberId);
   }
 
   @GetMapping("/history/me")
   @Operation(summary = "내가 보냈던 메세지 목록 확인하기")
-  public List<ChatMessage> getMyMessages(@AuthenticationPrincipal UserDetails userDetails) {
+  public List<ChatMessage> getMyMessages(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    Long memberId = customUserDetails.getId();
     return chatMessageService.getMyMessages(memberId);
   }
 
   @GetMapping("/history/{roomId}")
   @Operation(summary = "특정 채팅방의 과거 메세지 불러오기")
-  public List<ChatMessage> getMessageHistory(@PathVariable("roomId") Long roomId) {
-    return chatMessageService.getMessageHistory(roomId);
+  public List<ChatMessageBroadcastDto> getMessageHistory(@PathVariable("roomId") Long roomId,
+      @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    Long memberId = customUserDetails.getId();
+
+    return chatMessageService.getMessageHistory(memberId, roomId);
   }
 }
