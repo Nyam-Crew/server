d6fd42de632f1db7b0a3f6061a6995cf3bcbc415
LteFroggy <zac0328@naver.com>
2025-08-13 14:43:48 +0900

    feat : 알림 관련 파일명, 파일 위치 변경 (#65)

--- everyday/src/main/java/com/nyam/everyday/common/alert/dto/AlertDto.java
+++ everyday/src/main/java/com/nyam/everyday/common/alert/dto/AlertDto.java
@@ -1,10 +1,10 @@
-package com.nyam.everyday.web.notification.dto;
+package com.nyam.everyday.common.alert.dto;
 
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class NotificationDto {
+public class AlertDto {
   private String content;
 }
