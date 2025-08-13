78f2a1c8c3d9bae1fb5723482bc4fea031642ebc
Ljeelra <dlwldms9803@gmail.com>
2025-08-13 10:57:06 +0900

    feat : 그룹 실시간 피드 서비스 생성  #62
    - develop 풀받아오기 위한 커밋

---
+++ everyday/src/main/java/com/nyam/everyday/module/team/service/TeamActivityFeedService.java
@@ -0,0 +1,18 @@
+package com.nyam.everyday.module.team.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * 그룹 실시간 피드 service
+ *
+ * @author : 이지은
+ * @fileName : TeamActivityFeedService
+ * @since : 25. 8. 13.
+ *
+ */
+@Service
+@RequiredArgsConstructor
+public class TeamActivityFeedService {
+}
