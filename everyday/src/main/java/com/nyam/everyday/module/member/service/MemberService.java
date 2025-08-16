be15d095fc0889e2d0c5227c295477c07a278167
juju <hellojuju558@gmail.com>
2025-08-16 23:44:38 +0900

    [feature] Member 연속 로그인 기능 추가

--- everyday/src/main/java/com/nyam/everyday/module/member/service/MemberService.java
+++ everyday/src/main/java/com/nyam/everyday/module/member/service/MemberService.java
@@ -15,6 +15,8 @@ import com.nyam.everyday.web.member.dto.MemberResponseDto;
 import com.nyam.everyday.web.member.dto.NicknameDuplicationResponse;
 import com.nyam.everyday.web.member.mapper.MemberMapper;
 import jakarta.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -89,4 +91,40 @@ public class MemberService {
     return new NicknameDuplicationResponse(isDuplicate);
   }
 
+
+
+  /** 로그인 연속 출석 정보 업데이트 */
+  @Transactional
+  public void updateLoginInfo(Long memberId) {
+    Member member = memberRepository.findById(memberId)
+        .orElseThrow(() -> new BaseException(ErrorCode.MEMBER_NOT_FOUND, "로그인 정보 업데이트 중 회원을 찾을 수 없습니다."));
+
+    LocalDateTime now = LocalDateTime.now();
+    LocalDateTime lastLogin = member.getLastLoginDate();
+
+    if (lastLogin == null) {
+      // 1. 첫 로그인이거나, 기능 추가 후 첫 로그인인 경우
+      member.setConsecutiveLoginDays(1);
+      log.info("첫 로그인 memberId : {}", memberId);
+    } else {
+      // 2. 마지막 로그인 날짜와 오늘 날짜를 비교 (시간은 제외하고 날짜만 비교)
+      LocalDate todayDate = now.toLocalDate();
+      LocalDate lastLoginDate = lastLogin.toLocalDate();
+
+      // 같은 날에 여러 번 로그인하는 경우는 횟수를 올리지 않음
+      if (!lastLoginDate.isEqual(todayDate)) {
+        if (lastLoginDate.isEqual(todayDate.minusDays(1))) {
+          // 1. 마지막 로그인이 어제인 경우 (연속 로그인)
+          member.setConsecutiveLoginDays(member.getConsecutiveLoginDays() + 1);
+          log.info("연속 로그인 memberId : {}, 연속 날짜 : {}", memberId, member.getConsecutiveLoginDays());
+          // TODO 연속 날짜 뱃지 대상인지 체크 로직 추가해야함 (ex. 7일 이상 연속출석)
+        } else {
+          // 2. 연속 로그인이 끊긴 경우
+          member.setConsecutiveLoginDays(1);
+        }
+      }
+    }
+    // 마지막 로그인 시간을 현재 시간으로 업데이트
+    member.setLastLoginDate(now);
+  }
 }
