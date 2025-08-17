273904c5a1f0162663978707ab037c6b400abf60
juju <hellojuju558@gmail.com>
2025-08-17 13:46:47 +0900

    [feature] 실시간 랭킹 기능 추가
    - 팀간, 팀내부 실시간 랭킹 기능
    - 뱃지 생성 관련 이미지파일 dto 수정

---
+++ everyday/src/main/java/com/nyam/everyday/web/badge/dto/BadgeResponseDto.java
@@ -0,0 +1,36 @@
+package com.nyam.everyday.web.badge.dto;
+
+import com.nyam.everyday.module.badge.entity.BadgeType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "뱃지 정보")
+public class BadgeResponseDto {
+
+    @Schema(description = "뱃지 이름", example = "우수회원")
+    private String name;
+
+    @Schema(description = "뱃지 설명", example = "10일 연속 출석한 우수회원에게 부여되는 뱃지입니다.")
+    private String description;
+
+    @Schema(description = "뱃지 타입 (REGULAR_CHALLENGE, EVENT_CHALLENGE)", example = "REGULAR_CHALLENGE")
+    private BadgeType badgeType;
+
+    @Schema(description = "뱃지 이미지 파일 - 없으면 기본 이미지로 생성", example = "[binary file]")
+    private MultipartFile badgeImageFile;
+
+    @Schema(description = "뱃지 이미지 url (최조 등록시에는 입력안함_S3에 저장된 이미지 url 리턴)", example = "https://s3.amazonaws.com/badges/badge123.png")
+    private String badgeImage;
+
+    @Schema(description = "뱃지 생성 날짜")
+    private LocalDateTime createdDate;
+}
