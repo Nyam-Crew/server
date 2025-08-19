cc2be6c03672efb86a1b59544614eb58a427609a
LteFroggy <zac0328@naver.com>
2025-08-19 11:02:31 +0900

    feat(#89) : Postman 기반으로 Notification을 생성하기 위한 NotificationTestDto 추가

---
+++ everyday/src/main/java/com/nyam/everyday/web/notification/dto/NotificationTestDto.java
@@ -0,0 +1,10 @@
+package com.nyam.everyday.web.notification.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class NotificationTestDto {
+  private String content;
+}
