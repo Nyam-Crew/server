5e6eeb34d98cd8275f3791fde28a811442159f5c
soheejjang0912 <66730047+soheejjang0912@users.noreply.github.com>
2025-08-20 12:22:56 +0900

    feat: 프론트 관련 api 추가 및 수정 #108

---
+++ everyday/src/main/java/com/nyam/everyday/module/meal/type/MealType.java
@@ -0,0 +1,10 @@
+package com.nyam.everyday.module.meal.type;
+
+/**
+ * @author : 장소희
+ * @fileName : MealType
+ * @since : 25. 8. 20.
+ */
+public enum MealType {
+    BREAKFAST, LUNCH, DINNER, SNACK
+}
