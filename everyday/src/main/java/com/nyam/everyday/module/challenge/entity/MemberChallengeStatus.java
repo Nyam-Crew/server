9a72bdf1f96c8ac4a01c14ca1afff40b699f6cf2
LteFroggy <zac0328@naver.com>
2025-08-21 10:35:19 +0900

    feat(#104) : 사용자의 챌린지별 완료 현황 등을 저장하기 위한 MemberChallengeStatus Entity, Repository 수정
    - 미완성된 부분 완성
    - ProgressCount를 갱신해야 하기에 setter 생성
    - ProgressCount를 1씩 증가하는 방식에서 MCD의 갯수를 세고 적용하는 방법으로 변경함에 따른 addProgressCount 삭제

--- everyday/src/main/java/com/nyam/everyday/module/challenge/entity/MemberChallengeStatus.java
+++ everyday/src/main/java/com/nyam/everyday/module/challenge/entity/MemberChallengeStatus.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Comment;
 
 @Entity
@@ -44,15 +45,11 @@ public class MemberChallengeStatus extends BaseEntity {
 
   @Column(nullable = false)
   @Builder.Default
+  @Setter
   private Integer progressCount = 0;
 
   // 클리어 처리할 떄 사용
   public void setAsCleared() {
     this.isCleared = true;
-  }
-
-  // Progress count 1 증가시킬 때 사용
-  public void addProgressCount() {
-    this.progressCount++;
   }
 }
--- everyday/src/main/java/com/nyam/everyday/module/challenge/repository/MemberChallengeStatusRepository.java
+++ everyday/src/main/java/com/nyam/everyday/module/challenge/repository/MemberChallengeStatusRepository.java
@@ -1,18 +1,19 @@
 package com.nyam.everyday.module.challenge.repository;
 
-import com.nyam.everyday.module.challenge.entity.ChallengeCode;
 import com.nyam.everyday.module.challenge.entity.MemberChallengeStatus;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberChallengeStatusRepository extends JpaRepository<MemberChallengeStatus, Long> {
 
   @Query("SELECT mcs "
-      + "FROM ")
-  Optional<MemberChallengeStatus> getMCSByMemberChallenge(Long memberId, Long challengeId);
+      + "FROM MemberChallengeStatus mcs "
+      + "WHERE mcs.member.memberId = :memberId AND mcs.challenge.id = :challengeId")
+  Optional<MemberChallengeStatus> getMCSByMemberChallenge(@Param("memberId") Long memberId, @Param("challengeId") Long challengeId);
 
   @Query("SELECT mcs.isCleared "
       + "FROM MemberChallengeStatus mcs "
