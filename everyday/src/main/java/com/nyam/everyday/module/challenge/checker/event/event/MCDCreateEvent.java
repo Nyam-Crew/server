8fd058f53a95d9f664e4cb730681c5eded28c874
LteFroggy <zac0328@naver.com>
2025-08-21 10:32:49 +0900

    feat(#104) : 사용자가 특정 날짜에 챌린지 조건을 만족했을 때 생성되는 MemberChallengeDay를 생성하는 Event, Listener 생성

---
+++ everyday/src/main/java/com/nyam/everyday/module/challenge/checker/event/event/MCDCreateEvent.java
@@ -0,0 +1,16 @@
+package com.nyam.everyday.module.challenge.checker.event.event;
+
+import com.nyam.everyday.module.challenge.entity.Challenge;
+import com.nyam.everyday.module.member.entity.Member;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MCDCreateEvent {
+
+  Member member;
+  Challenge challenge;
+  LocalDate targetDate;
+}
