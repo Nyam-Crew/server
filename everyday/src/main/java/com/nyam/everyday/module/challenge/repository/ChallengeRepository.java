8752fc1063f21e4c48e36d8337408c69abf3d9a3
LteFroggy <zac0328@naver.com>
2025-08-21 10:37:36 +0900

    feat(#104) : ChallengeRepository에서 Entity명에 맞도록 함수명 수정
    - ChallengeType -> Type으로 함수명 변경

--- everyday/src/main/java/com/nyam/everyday/module/challenge/repository/ChallengeRepository.java
+++ everyday/src/main/java/com/nyam/everyday/module/challenge/repository/ChallengeRepository.java
@@ -3,13 +3,12 @@ package com.nyam.everyday.module.challenge.repository;
 import com.nyam.everyday.module.challenge.entity.Challenge;
 import com.nyam.everyday.module.challenge.entity.ChallengeCode;
 import com.nyam.everyday.module.challenge.entity.ChallengeTag;
-import com.nyam.everyday.module.challenge.entity.ChallengeType;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
-  Challenge findByChallengeCode(ChallengeCode challengeCode);
+  Challenge getByChallengeCode(ChallengeCode challengeCode);
 
-  List<Challenge> getAllByChallengeType(ChallengeTag challengeTag);
+  List<Challenge> getAllByType(ChallengeTag challengeTag);
 }
