31043ed21a5a6839cca85fab9751347e6d15071f
juju <hellojuju558@gmail.com>
2025-08-23 18:23:20 +0900

    [fix] 마이페이지 페이징 순서 수정
    - 페이징 순서 이슈 수정

---
+++ everyday/src/main/java/com/nyam/everyday/web/member/dto/BadgeCountResponse.java
@@ -0,0 +1,18 @@
+package com.nyam.everyday.web.member.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "뱃지 개수")
+public class BadgeCountResponse {
+
+  @Schema(description = "뱃지 개수", example = "0")
+  private long count;
+}
