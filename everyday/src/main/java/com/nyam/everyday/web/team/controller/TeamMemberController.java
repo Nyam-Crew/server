f9869c694d76de422ba86aff92599ceda6fe951a
Ljeelra <dlwldms9803@gmail.com>
2025-08-25 06:22:41 +0900

    feat: 내 그룹 정보 조회 API 추가  #122
    - 경로 "/me/my-list", getMyPageGroupList()
    - 원래는 /my-page로 경로 설정하였으나 TeamController와 TeamMemberController의 RequestMapping이 동일하여 경로 충돌이 발생함.

--- everyday/src/main/java/com/nyam/everyday/web/team/controller/TeamMemberController.java
+++ everyday/src/main/java/com/nyam/everyday/web/team/controller/TeamMemberController.java
@@ -4,10 +4,7 @@ import com.nyam.everyday.common.exception.BaseException;
 import com.nyam.everyday.common.exception.ErrorCode;
 import com.nyam.everyday.module.team.service.TeamMemberService;
 import com.nyam.everyday.security.core.CustomUserDetails;
-import com.nyam.everyday.web.team.dto.MemberTeamListDto;
-import com.nyam.everyday.web.team.dto.TeamBanDto;
-import com.nyam.everyday.web.team.dto.TeamRoleChangeDto;
-import com.nyam.everyday.web.team.dto.TeamTransLeaderDto;
+import com.nyam.everyday.web.team.dto.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +18,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 그룹 멤버 관리에 대한 컨트롤러
@@ -105,4 +104,11 @@ public class TeamMemberController {
     return ResponseEntity.ok(teamList);
   }
 
+    @GetMapping("/me/my-list")
+    @Operation(summary = "내 그룹 페이지 정보 조회", description = "자신이 가입했거나 신청한 모든 그룹의 상세 정보를 가져옵니다.")
+    public ResponseEntity<List<TeamDetailDto>> getMyPageGroupList(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long memberId = userDetails.getId();
+        List<TeamDetailDto> myGroupsInfo = teamMemberService.getMyPageGroups(memberId);
+        return ResponseEntity.ok(myGroupsInfo);
+    }
 }
