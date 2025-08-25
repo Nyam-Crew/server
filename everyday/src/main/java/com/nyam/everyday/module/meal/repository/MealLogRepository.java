d98fd2c40a95818d1eaac33e41145ef83cc7586b
LteFroggy <zac0328@naver.com>
2025-08-25 17:09:11 +0900

    feat(#139) : MealLogRepository에 특정 유저가 등록한 Meallog 갯수 불러오는 기능 추가

--- everyday/src/main/java/com/nyam/everyday/module/meal/repository/MealLogRepository.java
+++ everyday/src/main/java/com/nyam/everyday/module/meal/repository/MealLogRepository.java
@@ -54,4 +54,10 @@ public interface MealLogRepository extends JpaRepository<MealLog, Long> {
     //특정 멤버의 특정 날짜, 특정 식사 타입에 해당하는 모든 기록을 생성일 오름차순으로 조회합니다. (피드 score 계산을 위해)
     List<MealLog> findByMember_MemberIdAndMealLogDateAndMealTypeOrderByCreatedDateAsc(Long memberId, Date date, MealType mealType);
 
+
+    // 특정 유저가 등록한 Meal Log 갯수를 셉니다 (챌린지 체크용)
+    @Query("SELECT count(ml) "
+        + "FROM MealLog ml "
+        + "WHERE ml.member.memberId = :memberId")
+    Long getCountByMemberId(@Param("memberId") Long memberId);
 }
