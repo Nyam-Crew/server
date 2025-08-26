1703199ad4f860bc572a0da269356115e019f747
LteFroggy <zac0328@naver.com>
2025-08-26 14:48:09 +0900

    feat(#139) : EventChallengeDto 추가됨에 따른 ChallengeDto -> RegularChallengeDto 파일명 변경

--- everyday/src/main/java/com/nyam/everyday/web/challenge/dto/RegularChallengeDto.java
+++ everyday/src/main/java/com/nyam/everyday/web/challenge/dto/RegularChallengeDto.java
@@ -1,18 +1,16 @@
 package com.nyam.everyday.web.challenge.dto;
 
-import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class ChallengeDto {
+public class RegularChallengeDto {
 
+  private Long challengeId;
   private String title;
   private String description;
   private boolean cleared;
   private Long targetCount;
   private Long progressCount;
-  private LocalDate startDate;
-  private LocalDate endDate;
 }
